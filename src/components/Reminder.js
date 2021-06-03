ebdead728939d4896d80be651768e030a96f4d1c
Ryan Andrews <rcandrews44@gmail.com>
2021-06-03 15:28:34 -0400

    added date to reminder

---
+++ src/components/Reminder.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+class Reminder extends Component {
+    state = {
+        reminder_text: this.props.reminder.reminder_text,
+        edit: false,
+        newReminder: "",
+        archived: false
+    }
+
+
+
+    toggleEdit = (e) => {
+        e.preventDefault()
+        this.setState({
+            edit: !this.state.edit,
+        })
+    }
+    toggleOff = (e) => {
+        // debugger
+        console.log(e.target.className)
+        this.state.edit !== false
+            && e.target.className !== "inputText"
+            && this.setState({
+                edit: !this.state.edit,
+            })
+    }
+
+    handleUpdatedReminder = (updatedReminder) => {
+        this.setState({
+            newReminder: updatedReminder,
+        })
+    }
+
+    render() {
+        return (
+            <div onClick={(e) => this.toggleOff(e)}
+                key={this.props.reminder.id} className='note'>
+
+                <span>
+                    {/* REMINDER TITLE */}
+                    <h3 className='title'>{this.props.reminder.title}</h3>
+                    <br></br>
+
+                    {/* DELETE REMINDER */}
+                    <button onClick={() => this.props.deleteReminder(this.props.reminder)}>X</button>
+                </span>
+
+                {/* <img src={this.props.note.image}></img> */}
+
+                {/* EDIT REMINDER */}
+                {this.state.edit
+                    ? <form onSubmit={(e) => this.props.updateReminder(e, this.state.newReminder, this.props.reminder)}>
+                        <input
+                            type="text"
+                            defaultValue={this.state.reminder_text}
+                            //    name="updatedNote" 
+                            className="inputText"
+                            //    value={this.state.updatedNote}
+                            onChange={(e) => this.handleUpdatedReminder(e.target.value)}
+                        ></input>
+                    </form>
+                    : <h4 className={this.state.reminder_text} onClick={(e) => this.toggleEdit(e)}>{this.state.reminder_text}</h4>}
+
+                {/* onClick everywhere that isn't the form changes state */}
+                {/* ARCHIVE BUTTON AND TIMESTAMP */}
+                <h4>{this.props.reminder.datetime}</h4>
+
+                <span>
+                    <button onClick={() => this.props.archiveReminder(!this.props.reminder.archive, this.props.reminder,
+                        this.props.reminder.archived !== null ? alert("Archived!") : alert("Unarchived :("))}>Archive</button>
+                </span>
+            </div>
+        );
+    }
+}
+
+export default Reminder;
