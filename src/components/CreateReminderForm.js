26c681c32f250285c1cf8f6c629963ed7e93dd0a
Ryan Andrews <rcandrews44@gmail.com>
2021-06-03 15:20:20 -0400

    updated reminder form with calendar and clock

--- src/components/CreateReminderForm.js
+++ src/components/CreateReminderForm.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import DatePicker from 'react-datepicker';
 
-class CreateReminderForm extends React.Component{
-    
-    state = {
-        title: "",
-        // dateTime: "",
-        note:"",
-        // details: "",
-        deadline: ""
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+class CreateReminderForm extends React.Component {
+
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            // dateTime: "",
+            reminder_text: "",
+            // details: "",
+            deadline: "",
+            startDate: new Date()
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        })
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.startDate)
+    }
+
+
     handleSubmit = (e) => {
-        
+
         this.setState({
             [e.target.name]: e.target.value
         })
@@ -20,34 +43,48 @@ class CreateReminderForm extends React.Component{
 
     render() {
         // console.log(this.props.posts)
-        return(
-            <div className={"noteForm"}>
+        return (
+            <div className={"reminderForm"}>
                 {/* <h3>Make a note or reminder</h3> */}
-               <form onSubmit={(e)=> this.props.addReminder(e, this.state)}>
-                   
-                   <input 
-                   type="text" 
-                   placeholder="Title" 
-                   name="title" 
-                   value={this.state.title}
-                   onChange={(e)=>this.handleSubmit(e)}
-                   ></input>
+                <form onSubmit={(e) => this.props.addReminder(e, this.state)}>
 
-                   {/* <input type="text" placeholder="image URL " name="image"
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={this.state.title}
+                        onChange={(e) => this.handleSubmit(e)}
+                    ></input>
+
+                    {/* <input type="text" placeholder="image URL " name="image"
                    value={this.state.image}
                    onChange={(e)=>this.handleSubmit(e)}
                    ></input> */}
                     <br></br>
-                   <input 
-                   type="text" 
-                   placeholder="Note" 
-                   name="note" 
-                   value={this.state.note}
-                   onChange={(e)=>this.handleSubmit(e)}
-                   ></input>
+                    <input
+                        type="text"
+                        placeholder="Reminder"
+                        name="reminder_text"
+                        value={this.state.reminder_text}
+                        onChange={(e) => this.handleSubmit(e)}
+                    ></input>
+                    <div className="form-group">
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                            name="startDate"
+                            showTimeSelect
+                            timeFormat="HH:mm"
+                            timeIntervals={20}
+                            timeCaption="time"
+                            dateFormat="MMMM d, yyyy h:mm aa"
 
-                   <button type="submit" className="submit-comment" >Submit</button>
-               </form>
+                        />
+
+                    </div>
+
+                    <button type="submit" className="submit-comment" >Submit</button>
+                </form>
             </div>
         )
     }
